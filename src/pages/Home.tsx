30b34cba2e24e06defd39d3e5ec1dd1fc241ccf2
Bruninho Volotão <bruninhovolotao@gmail.com>
2025-05-07 16:36:51 -0300

    insert vercel insigths

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import '../styles/global.css'
 import 'animate.css';
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const projetos = [
     {
@@ -33,6 +34,7 @@ const projetos = [
 export function Home(){
     return(
         <>
+        <SpeedInsights/>
         <Header />
         <Box component={"section"} className="section-hero">
             <Container>
