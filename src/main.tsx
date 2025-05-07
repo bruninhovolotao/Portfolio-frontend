d722def83e08cf2466b517168910451ea0e9ed8c
Bruninho Volotão <bruninhovolotao@gmail.com>
2025-05-07 16:59:42 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AppRoutes } from './routes/index.tsx'
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SpeedInsights/>
     <AppRoutes />
   </StrictMode>,
 )
